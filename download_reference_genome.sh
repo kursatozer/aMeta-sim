97b3ad54c1f8ccb531c714d3af7ab0fdb155dd25
kursatozer <f.kursatozer@gmail.com>
2023-03-19 22:57:14 +0300

    fixed download.referance

--- download_reference_genome.sh
+++ download_reference_genome.sh
@@ -4,26 +4,24 @@
 # ./download_reference_genome.sh
 
 
-# endo
-
-wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_genomic.fna.gz
 
 # bact
-wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/006/765/GCF_000006765.1_ASM676v1/GCF_000006765.1_ASM676v1_genomic.fna.gz
+wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/457/555/GCF_001457555.1_NCTC10562/GCF_001457555.1_NCTC10562_genomic.fna.gz
+wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/144/405/GCF_000144405.1_ASM14440v1/GCF_000144405.1_ASM14440v1_genomic.fna.gz
+wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/022/819/245/GCF_022819245.1_ASM2281924v1/GCF_022819245.1_ASM2281924v1_genomic.fna.gz
 
 # cont
-
 wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/900/460/135/GCF_900460135.1_41686_D01/GCF_900460135.1_41686_D01_genomic.fna.gz
+wget ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/010/605/GCF_000010605.1_ASM1060v1/GCF_000010605.1_ASM1060v1_genomic.fna.gz
 
-# endo
-
-gunzip GCF_000222975.1_ASM22297v1_genomic.fna.gz
-gunzip GCF_000195955.2_ASM19595v2_genomic.fna.gz
 
 # bact
-
-gunzip GCF_000006765.1_ASM676v1_genomic.fna.gz 
+gunzip GCF_001457555.1_NCTC10562_genomic.fna.gz
+gunzip GCF_000144405.1_ASM14440v1_genomic.fna.gz
+gunzip GCF_022819245.1_ASM2281924v1_genomic.fna.gz
 
 # cont
-
 gunzip GCF_000005845.2_ASM584v2_genomic.fna.gz
+gunzip GCF_900460135.1_41686_D01_genomic.fna.gz
+gunzip GCF_000010605.1_ASM1060v1_genomic.fna.gz
