1e55fdb0bdf9823bcbdfc4bbbd7a6949647d8d32
kursatozer <f.kursatozer@gmail.com>
2023-03-31 15:07:48 +0300

    added bacteria list and dowload.sh

---
+++ download.sh
@@ -0,0 +1,22 @@
+
+# first download assembly summary
+wget -nc https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt
+
+# get only necessary part
+cut -f 1,5,6,8,20 assembly_summary.txt | grep -wv na > only_bacteria.txt
+
+mkdir -p bacteria
+
+while read BACTERIA
+do 
+	
+	# get only assembly summary ID
+	ID=$(grep "${BACTERIA}" only_bacteria.txt | cut -f5 | cut -d "/" -f10)
+
+	# get ftp link
+	LINK=$(grep "${BACTERIA}" only_bacteria.txt | cut -f5 | sed 's/https/ftp/g')
+
+	# download ftp
+	wget  ${LINK}/${ID}_genomic.fna.gz -O bacteria/${ID}_genomic.fna.gz
+
+done < bacteria.list
