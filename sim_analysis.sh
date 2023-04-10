3fce72c9e866e4f4e7d5d40459ff13221a27292f
Emrah Kırdök <emrahkirdok@gmail.com>
2023-04-10 17:27:32 +0300

    sim analysis update

--- sim_analysis.sh
+++ sim_analysis.sh
@@ -8,11 +8,12 @@ cutadapt -q 20 -m 30 --trim-n -a AGATCGGAAGAG -A AGATCGGAAGAG -j 4 -o data/simul
 fastqc data/simulation_s1_processed.fq.gz data/simulation_s2_processed.fq.gz
 
 # align bidirectional reading with flash for longer readings
-cd data/
-flash -m11 -M15 -z simulation_s1_processed.fq.gz simulation_s2_processed.fq.gz 2>&1 | tee flash.log
-cd ../
 
+mkdir -p results/merged-fastq
 
+flash -m11 -M15 -z data/simulation_s1_processed.fq.gz data/simulation_s2_processed.fq.gz --output-directory results/merged-fastq/ --output-prefix simulation 2>&1 | tee flash.log
+
+zcat results/merged-fastq/simulation.extendedFrags.fastq.gz results/merged-fastq/simulation.notCombined_1.fastq.gz results/merged-fastq/simulation.notCombined_2.fastq.gz | gzip >results/merged-fastq/simulation.merged.fastq.gz  
 
 
 
