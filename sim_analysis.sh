569ea9421f695a9554ed03456a5bf9dd06aa83a2
kursatozer <f.kursatozer@gmail.com>
2023-04-10 05:34:30 +0300

    sim analysis upgreded

--- sim_analysis.sh
+++ sim_analysis.sh
@@ -1,26 +1,14 @@
 # Quality control with fastqc
 fastqc data/simulation_s1.fq.gz data/simulation_s2.fq.gz 
 
-# Convert zip formatted files to gz format
-tar -zcvf simulation_s1_fastqc.gz simulation_s1_fastqc.zip
-tar -zcvf simulation_s1_fastqc.gz simulation_s2_fastqc.zip
-
-# Transfer files with cutadapt applied to another folder
-mkdir data/cutadapt
-mv data/simulation_s1_fastqc.gz data/cutadapt
-mv data/simulation_s2_fastqc.gz data/cutadapt
-cp data/simulation_s1.fq.gz data/cutadapt
-cp data/simulation_s2.fq.gz data/cutadapt
-
 # Editing data with cutadapt
-cutadapt -q 28 -m 10 --trim-n -a AGATCGGAAGAG -A AGATCGGAAGAG -j 4 -o data/cutadapt/simulation_s1_fastqc.gz -p data/cutadapt/simulation_s2_fastqc.gz data/cutadapt/simulation_s1.fq.gz data/cutadapt/simulation_s2.fq.gz
+cutadapt -q 20 -m 30 --trim-n -a -z AGATCGGAAGAG -A AGATCGGAAGAG -j 4 -o data/cutadapt/data/simulation_s1.fq.gz -p data/cutadapt/data/simulation_s2.fq.gz
 
 # Quality control after cutadapt
 fastqc data/cutadapt/simulation_s1_fastqc.gz data/cutadapt/simulation_s2_fastqc.gz
 
 # align bidirectional reading with flash for longer readings
-cd data/cutadapt
-flash -m11 -M15 -z simulation_s1_fastqc.gz simulation_s2_fastqc.gz 2>&1 | tee flash.log
+flash -m11 -M15 -z  simulation_s1.fq.gz simulation_s2.fq.gz 2>&1 | tee flash.log
 
 
 
