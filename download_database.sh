5639577b0134be78be9f4560cfb5f804a9af1fef
kursatozer <f.kursatozer@gmail.com>
2023-04-30 16:24:48 +0300

    space deleted

--- download_database.sh
+++ download_database.sh
@@ -1,8 +1,10 @@
 # download database
+
 wget https://genome-idx.s3.amazonaws.com/kraken/standard_08gb_20230314/inspect.txt
 wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_20230314.tar.gz
 
 # database extraction
+
 gunzip k2_standard_20230314.tar.gz
 mkdir -p results/k2_standard_08gb_20230314
 tar -xvf k2_standard_08gb_20230314.tar -C ~/aMeta-sim/results/k2_standard_08gb_20230314
