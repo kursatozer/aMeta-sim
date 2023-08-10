6777fe8afddc05f772fc6a95a86f864ec689746f
Kürşat Özer <90481141+kursatozer@users.noreply.github.com>
2023-08-10 15:06:16 +0300

    Update sim_analysis.sh

--- sim_analysis.sh
+++ sim_analysis.sh
@@ -5,7 +5,9 @@ fastqc data/simulation_s1.fq.gz data/simulation_s2.fq.gz
 python3 -m cutadapt -q 20 -m 30 --trim-n -a AGATCGGAAGAG -A AGATCGGAAGAG -o data/simulation_s1_processed.fq.gz -p data/simulation_s2_processed.fq.gz data/simulation_s1.fq.gz data/simulation_s2.fq.gz
 
 # Quality control after cutadapt
+conda activate base
 fastqc data/simulation_s1_processed.fq.gz data/simulation_s2_processed.fq.gz
+conda activate aMetasim
 
 # align bidirectional reading with flash for longer readings
 mkdir -p results/merged-fastq
